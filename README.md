8cecf126e0ccaa1508db11670d37b27de508c1e0
rolv <rolv.seehuus@schibsted.com>
2021-10-15 09:56:44 +0200

    Version 0.0.0.0RC1/Alpha

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 Grpc-starter
 ============
 
-The intent with this is to provide a really minimal grpc-service, demonstrating how to set up a simple grpc-service written in Java, building it with Gradle using the Kotlin-dsl.
+The intent with this is to provide a really minimal grpc-service, demonstrating
+how to define and set up a simple grpc-service written in Java, building it
+with Gradle using the Kotlin-dsl.
 
-Most of the actual code is nicked directly from the helloworld example from the Grpc-distribution, the main contribution is to show what the minimum configuration required to compile and run it is.
+The 'service' used is the 'hello-world' example from the grpc-distribution.
+This examplifies the basics of defining value-objects and defining a
+service. 
+
+Most of the actual code is nicked directly from the helloworld example from the
+Grpc-distribution, the main contribution is to show what the minimum
+configuration required to compile and run it is.
 
 How take it for a test-drive:
 
@@ -12,8 +20,15 @@ How take it for a test-drive:
 
 This should start the server.
 
-Then in another terminal:
+Then in another terminal run the client.
 
 	$ ./app/build/install/app/bin/hello-world-client
 
 
+
+Further reading
+===============
+
+* GRPC [homepage](https://grpc.io)
+* The java version of [GRPC](https://github.com/grpc/grpc-java)
+* The [Gradle-plugin](https://github.com/google/protobuf-gradle-plugin)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,45 +3,41 @@ import com.google.protobuf.gradle.*
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    // How gradle works with protobufs
     id("com.google.protobuf") version "0.8.17"
     idea
 }
 
 repositories {
-    // Use JCenter for resolving dependencies.
-    mavenCentral()
     jcenter()
 }
 
 dependencies {
-    testImplementation("io.grpc:grpc-testing:1.15.1")
 
     // Use JUnit test framework.
     testImplementation("junit:junit:4.13")
-    //    testImplementation("org.mockito:mockito-core:3.4.0")
 
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:29.0-jre")
-
-    // Protobuf and grpc deps
+    // Protobuf and grpc dependencies
     implementation("com.google.protobuf:protobuf-java:3.6.1")
     implementation("io.grpc:grpc-stub:1.15.1")
     implementation("io.grpc:grpc-protobuf:1.15.1")
+    runtimeOnly("io.grpc:grpc-netty-shaded:1.15.1")
+    testImplementation("io.grpc:grpc-testing:1.15.1")
 }
 
-application {
-    // Define the main class for the application.
-    mainClass.set("helloGrpc.HelloWorldServer")
-}
 
 protobuf {
     protoc {
+        // Protoc - the protobuf-dsl compiler/code generator needs to be installed somehow.
+        // For our usage, this is probably the simplest and mots straight forward.
         artifact = "com.google.protobuf:protoc:3.6.0"
     }
 
     /*
-      It's necessary to first add the grpc-plugin, demonstrated in the 'plugins' section, and then 
-      enable the grpc-plugin where relevant, as done in the generateProtoTasks section.
+      Protoc only generate object-classes from the proto-files. In order to generate
+      servier/client stubs we also need gen-grpc plugin. This needs to be enabled where
+      relevant in the build. Below it's enabled everywhere - with a more complicated
+      build a more targeted approach might be warranted.
     */
 
     plugins {
@@ -55,8 +51,8 @@ protobuf {
     }
 }
 
-
-// Inform IDEs like IntelliJ IDEA, Eclipse or NetBeans about the generated code.
+// Not needed to get a valid build, but necessary in order to inform IDEs like
+// IntelliJ IDEA, Eclipse or NetBeans about the generated code.
 sourceSets {
     main {
         java {
@@ -66,15 +62,7 @@ sourceSets {
     }
 }
 
-
 tasks {
-    register<CreateStartScripts>("helloWorldServer"){
-        mainClassName="helloGrpc.HelloWorldServer"
-        applicationName="hello-world-server"
-        outputDir = file("$buildDir/tmp")
-        classpath = startScripts.get().classpath
-    }
-
     register<CreateStartScripts>("helloWorldClient"){
         mainClassName="helloGrpc.HelloWorldClient"
         applicationName="hello-world-client"
@@ -84,9 +72,11 @@ tasks {
 }
 
 application {
+    // Define the main class for the application.
+    applicationName = "hello-world-server"
+    mainClass.set("helloGrpc.HelloWorldServer")
     applicationDistribution.into("bin"){
         from(tasks.named("helloWorldClient"))
-        from(tasks.named("helloWorldServer"))
         fileMode = "0755".toInt(8)
     }
 }
